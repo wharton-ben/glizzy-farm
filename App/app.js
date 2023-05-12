b923281298d5fd191576b2b5afb44803d6495565
Ben <benwharton5088@gmail.com>
2023-05-11 22:36:15 -0500

    I am terrible at this stuff

--- App/app.js
+++ App/app.js
@@ -11,22 +11,59 @@ function actSilly(){
     anim.play();    
 };
 
-// I want to create a function to move the text off to the top of the screen when Glizzard is clicked
-
-// function moveTextOffScreen(){
-//     let textToMove = document.getElementById("main-text");
-//     let keyframes = [
-//         { transform: 'translateX(0px) translateY(10000px)'}
-//       ];
-//     let animation = new keyframeEffect(textToMove, keyframes, { duration: 10000 });
-//     let anim = new Animation(animation);
-//     anim.play();
-// 
-// };
-
-// document.getElementById("move-button").addEventListener("click", moveTextOffScreen);
-
 function freezeGlizzard(){
-    let glizzardText = document.getElementById("Glizzard-Title")
+    let glizzardText = document.getElementById("Glizzard-Title");
+    glizzardText.style.fontSize = ".5px";
     // I want to create a frozen theme for the text
+};
+
+function changeBackgroundColor(){
+    // let farmColor = document.getElementsByTagName("Body");
+    // farmColor[0].style.backgroundcolor = "green";
+    actSilly();
+    let bgcolor = document.getElementById("mainbody");
+    bgcolor.style.backgroundColor = "green";
+
+};
+
+function moveTextOffScreenUpward(){
+    let allTextOnScreen = document.getElementById("allText");
+    
+};
+
+function controlFarmerGlizBag(){
+    const glizbag = document.getElementById("farmer-glizbag");
+    let xPos = 0;
+    let yPos = 0;
+
+    document.addEventListener("keydown", function(event){
+        switch(event.key) {
+            case "ArrowUp":
+                yPos -= 10;
+                break;
+            case "ArrowDown":
+                yPos += 10;
+                break;
+            case "ArrowLeft":
+                xPos -= 10;
+                break;
+            case "ArrowRight":
+                xPos += 10;
+                break;
+            default:
+                return;
+        }
+
+        glizbag.style.top = yPos + "px";
+        glizbag.style.left = xPos + "px";
+    });
+
+};
+function playGame(){
+    // This will be the function that will start the game
+    let mainWindow = document.getElementById("mainbody");
+    mainWindow.innerHTML = "";
+    changeBackgroundColor();
+    mainWindow.innerHTML = '<img src="Images/glizbag.gif" alt="Glizbag" id="farmer-glizbag" onclick="controlFarmerGlizbag()"></img>';
+    controlFarmerGlizBag();
 };
